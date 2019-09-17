641b20d02d6a2566ff5c7d8de58e5ded345bdec0
Kevin Lioi <kevin@darwynhq.com>
2019-09-17 02:43:26 -0700

    [+] Add logic to check for existing .c files and conditionally run a cython build

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 from setuptools import setup, Extension
-
-from Cython.Build import cythonize
 import numpy
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cythonised_files = {}
+ext_modules = []
+include_dirs = []
+
+if use_cython:
+    ext_modules += [
+        Extension(name="genetic_functions", sources=["genopt/genetic_functions.pyx"]),
+    ]
+    include_dirs += [
+        numpy.get_include()
+    ]
+    cythonised_files = cythonize(ext_modules)
+else:
+    ext_modules += [
+        Extension(name="genetic_functions", sources=["genopt/genetic_functions.c"]),
+    ]
 
 setup(
     name='genopt',
@@ -12,8 +34,8 @@ setup(
     author_email='kevin@darwynhq.com',
     license='unlicense',
     packages=['genopt'],
-    ext_modules=cythonize(Extension(name="genetic_functions", sources=["genopt/*.pyx"])),
-    include_dirs=[numpy.get_include()],
+    ext_modules=cythonised_files,
+    include_dirs=include_dirs,
     install_requires=[
         'numpy',
         'Cython',
